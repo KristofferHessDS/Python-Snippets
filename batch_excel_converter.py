abdb542e425e86212d9f67611f0130f6aa3c86a1
Kristoffer Hess <124538780+KristofferHessDS@users.noreply.github.com>
2023-06-24 23:20:10 -0700

    Update batch_excel_converter.py

--- batch_excel_converter.py
+++ batch_excel_converter.py
@@ -6,26 +6,29 @@ xlsx_files = [f for f in os.listdir() if f.endswith('.xlsx')]
 
 # iterate over the list of files
 for file in xlsx_files:
-    # indicate which file is starting to be converted
-    print(f"Starting to convert {file}...")
+    try:
+        # indicate which file is starting to be converted
+        print(f"Starting to convert {file}...")
 
-    # read the Excel file
-    df = pd.read_excel(file, engine='openpyxl')
+        # read the Excel file
+        df = pd.read_excel(file, engine='openpyxl')
 
-    # get the file name without the extension
-    base_name = os.path.splitext(file)[0]
+        # get the file name without the extension
+        base_name = os.path.splitext(file)[0]
 
-    # specify the name of the csv file to be created
-    csv_file = base_name + '.csv'
-    
-    # if the csv file already exists, delete it first
-    if os.path.exists(csv_file):
-        os.remove(csv_file)
+        # specify the name of the csv file to be created
+        csv_file = base_name + '.csv'
 
-    # create a csv file from the dataframe
-    df.to_csv(csv_file, index=False)
-    
-    # indicate that the file has been converted
-    print(f"Converted {file} to {csv_file}!")
+        # if the csv file already exists, delete it first
+        if os.path.exists(csv_file):
+            os.remove(csv_file)
+
+        # create a csv file from the dataframe
+        df.to_csv(csv_file, index=False)
+
+        # indicate that the file has been converted
+        print(f"Converted {file} to {csv_file}!")
+    except Exception as e:
+        print(f"Error occurred while converting {file}: {str(e)}")
 
 print("All .xlsx files have been converted to .csv!")
